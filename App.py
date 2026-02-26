69285b1c47782e49fb831bb0f62a30a7b737fb9d
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-02-26 07:09:19 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from PTL import image
 
 st.title("CHRIS FROM TINDER HAS AWAKEN")
 
 st.header("Esta pagina es de prueba para FRAMEMOGGING")
 st.write("CLAV GOT MOGGED BY A ASU FRAT LEADER")
+
+image = Image.open('CHRIS FROM TINDER TEMU')
