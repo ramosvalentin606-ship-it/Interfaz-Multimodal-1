fce77031871c9084cdd1917ae133f600b113d07a
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-02-26 07:32:43 -0500

    Update App.py

--- App.py
+++ App.py
@@ -6,26 +6,38 @@ st.title("CHRIS FROM TINDER HAS AWAKEN")
 st.header("Esta pagina es de prueba para FRAMEMOGGING")
 st.write("CLAV GOT MOGGED BY A ASU FRAT LEADER")
 
+# Mostrar primera imagen
 image = Image.open('ARCANGEL_ABUSADOL.jpg')
-st.image(image,caption='Interfaces Multimodales')
+st.image(image, caption='Interfaces Multimodales')
 
-TEXTO = st.text_input('eSCRIbe AlGo')
-st.write('eL TexTO EsCRito es',texto)
+# Input de texto
+texto = st.text_input('eSCRIbe AlGo')
+st.write('eL TexTO EsCRito es:', texto)
 
 st.subheader("AHoRa UsARemos DoS CoLUMnas")
 
+# Crear columnas
+col1, col2 = st.columns(2)
+
 with col1:
-  st.subheader("TEz az első oszlop")
-  st.write("A multimodális interfészek javítják a felhasználói élményt")
-  resp = st.checkbox('Estoy de Acuerdo')
+    st.subheader("TEz az első oszlop")
+    st.write("A multimodális interfészek javítják a felhasználói élményt")
+    resp = st.checkbox('Estoy de Acuerdo')
 
 with col2:
-  st.subheader("Ez a második oszlop")
-  modo = st.radio("Melyik mód az elsődleges a kezelőfelületeden?", ('Vizuális','Auditív','Tapintható'))
-  if modo == 'Vizuális':
-    st.write('A vizuális megjelenés alapvető fontosságú a felhasználói felület szempontjából.')
-  if modo =='Auditív':
-    st.write('A hallás alapvető fontosságú a felhasználói felületen')
-  if modo == 'Tapintható':
-    st.write('Az érintés elengedhetetlen a kezelőfelületben')
-    Image.open('CHRIS FROM TINDER TEMU.jpeg')
+    st.subheader("Ez a második oszlop")
+    modo = st.radio(
+        "Melyik mód az elsődleges a kezelőfelületeden?",
+        ('Vizuális', 'Auditív', 'Tapintható')
+    )
+
+    if modo == 'Vizuális':
+        st.write('A vizuális megjelenés alapvető fontosságú a felhasználói felület szempontjából.')
+
+    elif modo == 'Auditív':
+        st.write('A hallás alapvető fontosságú a felhasználói felületen')
+
+    elif modo == 'Tapintható':
+        st.write('Az érintés elengedhetetlen a kezelőfelületben')
+        imagen2 = Image.open('CHRIS FROM TINDER TEMU.jpeg')
+        st.image(imagen2, caption='Modo táctil activado')
